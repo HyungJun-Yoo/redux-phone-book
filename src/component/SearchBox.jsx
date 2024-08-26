1115841ae3ba08f925692a5e1c5157a5e0e881c2
HyungJunYoo95 <u-jun11@nate.com>
2024-08-27 04:48:31 +0900

    style: 버튼 반응형 수정

--- src/component/SearchBox.jsx
+++ src/component/SearchBox.jsx
@@ -25,19 +25,19 @@ const SearchBox = () => {
         value={keyword}
         onChange={(e) => setKeyword(e.target.value)}
         placeholder='검색할 이름을 입력하세요...'
-        className='flex-1 p-2 border border-gray-300 rounded-l-md focus:outline-none focus:ring-2 focus:ring-gray-500'
+        className='flex-1 p-2 border border-gray-300 rounded-l-md focus:outline-none focus:ring-2 focus:ring-gray-500 mr-2 lg:mr-4'
       />
-      <div className='flex gap-4'>
+      <div className='flex gap-2 lg:gap-4'>
         <button
           type='submit'
-          className='w-[80px] bg-gray-500 text-white p-2 rounded-md hover:bg-gray-600 transition duration-200'
+          className='w-[60px] lg:w-[80px] bg-gray-500 text-white p-2 rounded-md hover:bg-gray-600 transition duration-200 text-nowrap'
         >
           검색
         </button>
         <button
           onClick={() => handleReset()}
           type='button'
-          className='w-[80px] bg-red-500 text-white p-2 rounded-md hover:bg-red-600 transition duration-200'
+          className='w-[60px] lg:w-[80px] bg-red-500 text-white p-2 rounded-md hover:bg-red-600 transition duration-200 text-nowrap'
         >
           검색 리셋
         </button>
